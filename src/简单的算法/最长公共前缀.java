6163814b4b40f4e324ee44f13d01f4301fd4dc0e
tom_pro <tz19950326@163.com>
2020-01-20 11:30:56 +0800

    有效括号

--- src/简单的算法/最长公共前缀.java
+++ src/简单的算法/最长公共前缀.java
@@ -45,3 +45,27 @@ public class 最长公共前缀 {
         return result;
     }
 }
+
+
+/**
+ * 官方题解
+ */
+class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        String ans = strs[0];
+        for(int i =1;i<strs.length;i++) {
+            int j=0;
+            for(;j<ans.length() && j < strs[i].length();j++) {
+                if(ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if(ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
+}
---
+++ src/简单的算法/有效的括号.java
@@ -0,0 +1,71 @@
+package 简单的算法;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class 有效的括号 {
+    public static void main(String[] args) {
+        String str = "((){}[[{)}";
+        String str2="()()()({[]})[{()}]";
+        System.out.println(isValid(str2));
+    }
+
+    public static boolean isValid(String str) {
+        if (str.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char res = str.charAt(i);
+            if (res == '(' || res == '[' || res == '{' && stack.size() < (str.length() - i)) {
+                stack.push(res);
+                continue;
+            }
+            if (!stack.empty() && res == map.get(stack.pop())) {
+                continue;
+            }
+            return false;   //字符串第一元素为'右括号'时，在这里直接返回false不用再去执行下一步判断，
+                            //否则虽然结果不影响，但是会做很多没有必要的判断。
+        }
+        return stack.empty();
+
+    }
+
+
+    /**
+     * 官方解题
+     */
+    public boolean isValid2(String s) {
+        assert s != null;
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c == '(' || c == '[' || c == '{') && stack.size() < s.length() - i) {
+                stack.push(c);
+                continue;
+            }
+            if (!stack.empty() && c == map.get(stack.pop())) {
+                continue;
+            }
+            return false;
+        }
+        return stack.empty();
+    }
+
+
+}
+
