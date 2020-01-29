6db0fe632b183231691ff8c9ea26fae0290f99e4
TangZhen <986947388@qq.com>
2020-01-29 21:26:28 +0800

    移除元素

---
+++ src/简单的算法/移除元素.java
@@ -0,0 +1,29 @@
+package 简单的算法;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ */
+public class 移除元素 {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        System.out.println(removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+//        Arrays.stream(nums).forEach(System.out::println);
+        return j;
+
+    }
+}
